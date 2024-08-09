5ad020aa02043de123f280abe3f905d1aeb8ab9e
MbaliNC <normam.cele@gmail.com>
2024-08-09 03:12:21 +0200

    Added axios and API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-function searchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("search-form-input");
-  let cityElement = document.querySelector("city");
-  cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit", searchSubmit);
+function searchCity(city) {
+  let apiKey = "0800e09bf035ao9a1ad4t33b0957a445";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
